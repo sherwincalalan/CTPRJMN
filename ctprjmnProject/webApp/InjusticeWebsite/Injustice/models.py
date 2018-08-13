ab995ffc6e834643b4f01de251e7ab698a1b185e
Ralph Bravante <ralph_bravante@dlsu.edu.ph>
2018-08-14 04:55:33 +0800

    DB TO

--- ctprjmnProject/webApp/InjusticeWebsite/Injustice/models.py
+++ ctprjmnProject/webApp/InjusticeWebsite/Injustice/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class twitterTweets(models.Model):
+	twUsername = models.CharField(max_length=20)
+	twContent = models.CharField(max_length=280)
+	twDate = models.DateTimeField('date published')
+	
+class 	facebookPosts(models.Model):
+	fbUsername = models.CharField(max_length=20)
+	fbContent = models.CharField(max_length=2000)
+	fbDate = models.DateTimeField('date published')
+
+class instagramPosts(models.Model):
+	igUsername = models.CharField(max_length=20)
+	igContent = models.CharField(max_length=300)
+	igDate = models.DateTimeField('date published')
+	
